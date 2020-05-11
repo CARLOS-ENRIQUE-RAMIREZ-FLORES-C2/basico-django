79c031187fc98719aae693708b3131f734fb7afb
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-10 23:38:19 -0500

    se dio funcionalidad al modelo de post y se agrego MEDIA_ROOT Y MEDIA_URL a settings.py

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#https://docs.djangoproject.com/en/2.0/ref/settings/#media-root
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL  =  '/media/'
--- platzigram/urls.py
+++ platzigram/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as posts_views
