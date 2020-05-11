2662045fd1db1da12ab577ee9a54a4fff30c61d0
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-11 01:05:38 -0500

    se restructuro el proyecto y se sacaron templates al path raiz

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+
 from platzigram import views as local_views
 from posts import views as posts_views
 
@@ -26,5 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('posts/', posts_views.list_posts)
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
