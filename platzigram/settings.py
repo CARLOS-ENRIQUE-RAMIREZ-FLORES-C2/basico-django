88f8300bae81d6bf006bbb02a9454bc4bd70ea9d
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-11 19:39:00 -0500

    se agrego el formulario de login

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -136,3 +136,5 @@ STATICFILES_FINDERS = [
 #https://docs.djangoproject.com/en/2.0/ref/settings/#media-root
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL  =  '/media/'
+
+LOGIN_URL = '/users/login'
